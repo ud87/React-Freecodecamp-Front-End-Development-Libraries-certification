1fbddc029c4b255377bee764a7ddd8d5c0998c56
Udaya <udayarai@hotmail.co.uk>
2024-07-03 22:48:16 +0100

    Timer.jsx component added

---
+++ 25+5 Clock/components/Timer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Timer() {
+  return (
+    <div className="timer">
+      <p id="timer-label">Session</p>
+      <p id="time-left">25</p>
+    </div>
+  );
+}
