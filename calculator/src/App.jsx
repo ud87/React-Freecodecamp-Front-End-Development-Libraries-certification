680088d41c8cc110adaa3dcdc1523c76850ff6a9
Udaya <udayarai@hotmail.co.uk>
2024-07-09 22:30:19 +0100

    Basic html and css grid layout for calculator added

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -6,7 +6,64 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <div className="container">
+      <div id="display">Display</div>
+      <div className="grid-container">
+        <button className="grid-item grid-col-span-2" id="clear">
+          AC
+        </button>
+        <button className="grid-item" id="divide">
+          /
+        </button>
+        <button className="grid-item" id="multiply">
+          X
+        </button>
+        <button className="grid-item" id="seven">
+          7
+        </button>
+        <button className="grid-item" id="eight">
+          8
+        </button>
+        <button className="grid-item" id="nine">
+          9
+        </button>
+        <button className="grid-item" id="subtract">
+          -
+        </button>
+        <button className="grid-item" id="four">
+          4
+        </button>
+        <button className="grid-item" id="five">
+          5
+        </button>
+        <button className="grid-item" id="six">
+          6
+        </button>
+        <button className="grid-item" id="add">
+          +
+        </button>
+        <button className="grid-item" id="one">
+          1
+        </button>
+        <button className="grid-item" id="two">
+          2
+        </button>
+        <button className="grid-item" id="three">
+          3
+        </button>
+        <button className="grid-item grid-row-span-2" id="equals">
+          =
+        </button>
+        <button className="grid-item grid-col-span-2" id="zero">
+          0
+        </button>
+        <button className="grid-item" id="decimal">
+          .
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
